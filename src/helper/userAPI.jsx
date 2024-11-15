4bf5ff1c0052b5e19da3388c61a56dfc5a8c14f1
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-11-15 08:07:30 +0530

    Update userAPI.jsx

--- src/helper/userAPI.jsx
+++ src/helper/userAPI.jsx
@@ -3,8 +3,12 @@ import axios from "axios";
 const fetchUserData = async () => {
   try {
     const res = await axios.get("https://car-management-backend-bm90.onrender.com/api/v1/users/profile", {
-      withCredentials: true,
-    });
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        });
     return res.data.data.user;
   } catch (error) {
     console.error("Error fetching user data:", error);
